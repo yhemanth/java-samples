b46433d644427dd476348f56b4c955eeebd07ed3
Hemanth Yamijala <yhemanth@gmail.com>
2014-09-26 12:11:14 +0530

    Family fun

--- src/main/java/com/thoughtworks/spikes/Mimi.java
+++ src/main/java/com/thoughtworks/spikes/Mimi.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.spikes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Mimi {
-    public static void main(String[] args) {
-        for (int i=0; i<8000; i++) {
-            System.out.println((i+1)+") Hello, Mimi");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        while (s != null) {
+            System.out.println("Hello, " + s + "!");
+            s = bufferedReader.readLine();
         }
     }
 }
