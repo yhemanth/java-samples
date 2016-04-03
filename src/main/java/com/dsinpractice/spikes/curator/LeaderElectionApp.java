64d281bb0a3319d526b8f9bf05c96d99ade36a44
Hemanth Yamijala <hyamijala@hortonworks.com>
2016-04-03 15:09:41 +0530

    Modified leader election example to take ACLs

--- src/main/java/com/dsinpractice/spikes/curator/LeaderElectionApp.java
+++ src/main/java/com/dsinpractice/spikes/curator/LeaderElectionApp.java
@@ -4,9 +4,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.ACLProvider;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
 import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LeaderElectionApp implements LeaderLatchListener {
 
@@ -17,21 +24,25 @@ public class LeaderElectionApp implements LeaderLatchListener {
     private final String zkHost;
     private final int zkPort;
     private String id;
+    private final String username;
+    private final String password;
     private LeaderLatch leaderLatch;
     private CuratorFramework curatorFramework;
 
-    public LeaderElectionApp(String zkHost, int zkPort, String id) {
+    public LeaderElectionApp(String zkHost, int zkPort, String id, String username, String password) {
         this.zkHost = zkHost;
         this.zkPort = zkPort;
         this.id = id;
+        this.username = username;
+        this.password = password;
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            LOG.info("Usage: java LeaderElectionApp <id>");
+        if (args.length != 3) {
+            LOG.info("Usage: java LeaderElectionApp <id> <username> <password>");
             System.exit(-1);
         }
-        LeaderElectionApp service = new LeaderElectionApp("localhost", 2181, args[0]);
+        LeaderElectionApp service = new LeaderElectionApp("localhost", 2181, args[0], args[1], args[2]);
         try {
             service.start();
         } catch (Exception e) {
@@ -55,7 +66,25 @@ public class LeaderElectionApp implements LeaderLatchListener {
 
     private void startLeaderElection() throws Exception {
         LOG.info("Starting leader election for " + id);
-        curatorFramework = CuratorFrameworkFactory.newClient(getConnectionString(),
+        curatorFramework = CuratorFrameworkFactory.builder().
+                connectString(getConnectionString()).
+                authorization("digest", (username + ":" + password).getBytes()).
+                aclProvider(new ACLProvider() {
+                    @Override
+                    public List<ACL> getDefaultAcl() {
+                        Id id = new Id("auth", username + ":" + password);
+                        return Arrays.asList(new ACL[]{new ACL(ZooDefs.Perms.ALL, id)});
+                    }
+
+                    @Override
+                    public List<ACL> getAclForPath(String s) {
+                        Id id = new Id("auth", username + ":" + password);
+                        return Arrays.asList(new ACL[]{new ACL(ZooDefs.Perms.ALL, id)});
+                    }
+                }).
+                retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME_MS, MAX_RETRIES)).build();
+
+                CuratorFrameworkFactory.newClient(getConnectionString(),
                 new ExponentialBackoffRetry(BASE_SLEEP_TIME_MS, MAX_RETRIES));
         curatorFramework.start();
         LOG.info("Starting leader latch for " + id);
