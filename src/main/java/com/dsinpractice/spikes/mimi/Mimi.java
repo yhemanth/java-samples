681e78b0563f0c1f45f15554270635d7bb651c6b
Hemanth Yamijala <hyamijala@hortonworks.com>
2016-11-09 23:13:33 +0530

    Changed from g to b

--- src/main/java/com/dsinpractice/spikes/mimi/Mimi.java
+++ src/main/java/com/dsinpractice/spikes/mimi/Mimi.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class Mimi {
     public static void main(String[] args) throws IOException {
-        String s = "g";
+        String s = "b";
         byte b = (byte) s.charAt(0);
         System.out.println((char)b);
     }
